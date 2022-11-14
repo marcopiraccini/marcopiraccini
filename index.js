7a69b6a781e142d5f6edfe2b1b01e0ce2d5dfce0
marcopiraccini <marco.piraccini@gmail.com>
2022-11-14 06:36:51 +0100

    fixed script

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import { readFileSync } from 'fs'
-import { join } from 'path'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 try {
   const output = readFileSync(join(__dirname, 'bin/output'), 'utf8')
   console.log(output)
 } catch (err) {
+  console.error(err)
   console.log('no output file found')
 }

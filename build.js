1e711c74a59ae8f88136c9afd3d8317689938e16
marcopiraccini <marco.piraccini@gmail.com>
2022-11-20 18:35:04 +0100

    fixed background and added mastodon

--- build.js
+++ build.js
@@ -10,17 +10,20 @@ const options = {
   padding: 1,
   margin: 1,
   borderStyle: 'round',
-  borderColor: 'green'
+  borderColor: 'green',
+  backgroundColor: 'black'
 }
 
-const banner = readFileSync('banner.txt').toString()
-const links = `${chalk.green('https://twitter.com/MarcoPiraccini')}
-${chalk.green('https://github.com/marcopiraccini')}`
+const banner = chalk.bgBlack(readFileSync('banner.txt').toString())
+const links = `${chalk.blue.bgBlack('marco.piraccini@gmail.com'.padEnd(59, ' '))}
+${chalk.blue.bgBlack('https://twitter.com/MarcoPiraccini'.padEnd(59, ' '))}
+${chalk.blue.bgBlack('https://fosstodon.org/@marcopiraccini'.padEnd(59, ' '))}
+${chalk.blue.bgBlack('https://github.com/marcopiraccini'.padEnd(59, ' '))}`
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// const [, ...trimmed] = output.split('\n')
 const card = chalk.green(boxen(banner + links, options))
+
 writeFileSync(join(__dirname, 'bin/output'), card)
 console.log(card)
